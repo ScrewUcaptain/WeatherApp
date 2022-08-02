4b2a91b967d582431a781327cae3f2b33561c79a
ScrewUcaptain <basilebrusseau@gmail.com>
2022-08-02 18:09:24 +0200

    working logical php part

--- public/index.php
+++ public/index.php
@@ -1,11 +1,15 @@
+<!DOCTYPE html>
 <?php
 
 require_once __DIR__ . '/../weatherAPI/curlRequest.php';
 $api = new Requester();
-$result = $api->APICall();
+if (isset($_GET['location'])) {
+    $location = $_GET['location'] ?? "Paris, France";
+    $result = $api->getData();
+    $iconURL = "http://openweathermap.org/img/wn/" . $result['currentDay']['iconWeather'] . "@2x.png";
+}
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -15,7 +19,7 @@ $result = $api->APICall();
     <title>SUc/Weatherapp</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Inter:wght@300;400;500;600&family=Poppins:wght@500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <script async defer src="js/script.js"></script>
 </head>
@@ -24,26 +28,93 @@ $result = $api->APICall();
     <!-- APP -->
     <section class="appLayout container">
         <!-- button for change C° to F° -->
-        <button class="switchDegree" onclick="">C°/F°</button>
+        <!-- <button class="switchDegree" onclick="">C°/F°</button> -->
         <!-- SEARCH AREA FOR USER INPUT -->
-        <h2 class="textSearch">Right now in, <input type="text" name="location" id="location" oninput="this.size = this.value.length + 1" placeholder="Paris, France"> it's <span class="weather"></span>.</h2>
-        <!-- THIS DAY -->
-        <div class="currentDay container">
-            <div class="currentDay-icon">A</div>
-            <div class="currentDay-Temp">B</div>
-            <div class="currentDay-Infos">
-                <div class="wind">a</div>
-                <div class="rain">b</div>
-                <div class="humidity">c</div>
+        <form action="">
+            <h2 class="textSearch">Right now in,
+                <input type="text" name="location" id="location" size="10" oninput="this.size = this.value.length" placeholder="Paris, France" value="<?= $location ?>"> <br>
+                it's <span class="weather"><?= $result['currentDay']['description']; ?></span>.
+                <button formmethod="GET" type="submit">GO ➔ </button>
+            </h2>
+            <!-- THIS DAY -->
+        </form>
+
+        <section class="currentDay container">
+
+            <div class="currentDay-icon">
+                <img class="currentIcon" src="<?= $iconURL ?>">
             </div>
-        </div>
+
+            <div class="currentDay-Temp">
+                <div class="temp actualTemp"><?= round($result['currentDay']['actualTemp'], 1) ?> C°</div>
+                <div class="temp twelveHTemp"><?= round($result['currentDay']['twelveHTemp'], 1) ?> C°</div>
+            </div>
+
+            <div class="currentDay-Infos">
+                <div class="wind">
+                    <p><?= $result['currentDay']['windSpeed'] . ' m/s' ?></p>
+                </div>
+                <div class="rain">
+                    <div class="prec">
+                        <?= $result['currentDay']['precipitation'] . ' %' ?>
+                    </div>
+                </div>
+                <div class="humidity">
+                    <div class="humResult"><?= $result['currentDay']['humidity'] . ' %' ?></div>
+                </div>
+            </div>
+        </section>
         <!-- OTHER DAYS -->
-        <div class="nextDays container">
+        <section class="nextDays container">
             <!-- TOMORROW -->
-            <div class="daysAfter"></div>
-            <div class="daysAfter"></div>
-            <div class="daysAfter"></div>
-        </div>
+            <div class="daysAfter day1">
+                <h4 class="nameDay_next"><?= Requester::timeDayMonth($result['nextDay']['time']) ?></h4>
+
+                <div class="afterday_icon">
+                    <img src="<?= "http://openweathermap.org/img/wn/" . $result['nextDay']['iconWeather'] . "@2x.png" ?>" alt="">
+                </div>
+
+                <div class="tempOtherDay">
+                    <h6 class="hourTemp"><?= Requester::timeHourMin($result['nextDay']['time']) ?></h6>
+                    <div class="afterday_temp0"><?= round($result['nextDay']['actualTemp'], 1) ?> C°</div>
+
+                    <h6 class="hourTemp"><?= Requester::timeHourMin($result['nextDay']['timePlus12']) ?></h6>
+                    <div class="afterday_temp12"><?= round($result['nextDay']['twelveHTemp'], 1) ?> C°</div>
+                </div>
+            </div>
+
+            <div class="daysAfter day2">
+                <h4 class="nameDay_next"><?= Requester::timeDayMonth($result['dayTwo']['time']) ?></h4>
+
+                <div class="afterday_icon">
+                    <img src="<?= "http://openweathermap.org/img/wn/" . $result['dayTwo']['iconWeather'] . "@2x.png" ?>" alt="">
+                </div>
+
+                <div class="tempOtherDay">
+                    <h6 class="hourTemp"><?= Requester::timeHourMin($result['dayTwo']['time']) ?></h6>
+                    <div class="afterday_temp0"><?= round($result['dayTwo']['actualTemp'], 1) ?> C°</div>
+
+                    <h6 class="hourTemp"><?= Requester::timeHourMin($result['dayTwo']['timePlus12']) ?></h6>
+                    <div class="afterday_temp12"><?= round($result['dayTwo']['twelveHTemp'], 1) ?> C°</div>
+                </div>
+
+            </div>
+
+            <div class="daysAfter day3">
+                <h4 class="nameDay_next"><?= Requester::timeDayMonth($result['dayThree']['time']) ?></h4>
+
+                <div class="afterday_icon">
+                    <img src="<?= "http://openweathermap.org/img/wn/" . $result['dayThree']['iconWeather'] . "@2x.png" ?>" alt="">
+                </div>
+                <div class="tempOtherDay">
+                    <h6 class="hourTemp"><?= Requester::timeHourMin($result['dayThree']['time']) ?></h6>
+                    <div class="afterday_temp0"><?= round($result['dayThree']['actualTemp'], 1) ?> C°</div>
+
+                    <h6 class="hourTemp"><?= Requester::timeHourMin($result['dayThree']['timePlus12']) ?></h6>
+                    <div class="afterday_temp12"><?= round($result['dayThree']['twelveHTemp'], 1) ?> C°</div>
+                </div>
+            </div>
+        </section>
     </section>
 
 </body>
--- weatherAPI/curlRequest.php
+++ weatherAPI/curlRequest.php
@@ -2,7 +2,7 @@
 
 class Requester
 {
-    public function APICall($city='Paris',$country='', $stateCode = '')
+    public function getLatLong($city='Paris',$country='', $stateCode = '')
     {
         if($stateCode !== '')
         {
@@ -31,13 +31,104 @@ class Requester
         {
             var_dump(curl_error($req));
         } else {
-            $data = json_decode($data);
+            $data = json_decode($data,true);
         }
         curl_close($req);
         // echo '<pre>';
         // var_dump($data);
         // echo '</pre>';
         // exit;
+        // $jsonResponse = [
+        //     $lat = $data['0]->lat
+        // ];
         return $data;
     }
+
+    public function weatherFromGeo($lat, $long)
+    {
+        $url = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$long&appid=f384d81b62089a0cb2f7bd55d0abab32&units=metric";
+
+        $req = curl_init($url);
+
+        curl_setopt_array($req, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false
+        ]);
+        $data = curl_exec($req);
+
+        if (!$data) {
+            var_dump(curl_error($req));
+        } else {
+            $data = json_decode($data, true);
+        }
+        curl_close($req);
+
+        return $data;
+    }
+
+    public function getData()
+    {
+        $temp = $_GET['location'];
+
+        // $tempArr = preg_split("/[\s,]+/", $temp);
+        $tempArr = explode(',', $temp);
+        $city = $tempArr[0] ?? '';
+        $country = $tempArr[1] ?? '';
+        $stateCode = $tempArr[2] ?? '';
+        $data = $this->getLatLong($city,$country,$stateCode);
+        $lat = $data['0']['lat'] ?? 48.8588897;
+        $long = $data['0']['lon'] ?? 2.3200410217200766;
+        
+        $data = $this->weatherFromGeo($lat,$long);
+        $precipitation = $data["list"][0]['pop'] * 100;
+    
+        $weatherData = [
+            'currentDay' => [
+                'main' => $data["list"][0]['weather'][0]['main'],
+                'description' => $data["list"][0]['weather'][0]['description'],
+                'iconWeather' => $data["list"][0]['weather'][0]['icon'],
+                'actualTemp' => $data["list"][0]['main']['temp'],
+                'twelveHTemp' => $data["list"][3]['main']['temp'],
+                'windSpeed' => $data["list"][0]['wind']['speed'],
+                'humidity' => $data["list"][0]['main']['humidity'],
+                'precipitation' => $precipitation,
+            ],
+            'nextDay' => [
+                'iconWeather' => $data["list"][8]['weather'][0]['icon'],
+                'actualTemp' => $data["list"][8]['main']['temp'],
+                'twelveHTemp' => $data["list"][12]['main']['temp'],
+                'time' => $data["list"][8]['dt'],
+                'timePlus12' => $data["list"][12]['dt'],
+            ],
+            'dayTwo' => [
+                'iconWeather' => $data["list"][16]['weather'][0]['icon'],
+                'actualTemp' => $data["list"][16]['main']['temp'],
+                'twelveHTemp' => $data["list"][20]['main']['temp'],
+                'time' => $data["list"][16]['dt'],
+                'timePlus12' => $data["list"][20]['dt'],
+            ],
+            'dayThree' => [
+                'iconWeather' => $data["list"][24]['weather'][0]['icon'],
+                'actualTemp' => $data["list"][24]['main']['temp'],
+                'twelveHTemp' => $data["list"][28]['main']['temp'],
+                'time' => $data["list"][24]['dt'],
+                'timePlus12' => $data["list"][28]['dt'],
+            ],
+        ];
+        // var_dump($this->convertToDate($data["list"][1]['dt']));
+        return $weatherData;
+    }
+
+    public static function timeHourMin($timestamp)
+    {
+        $time = date('H:i',$timestamp);
+        return $time;
+    }
+
+    public static function timeDayMonth($timestamp)
+    {
+        $time = date('d/m', $timestamp);
+        return $time;
+    }
+
 }
